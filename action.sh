664ccbb515b3406608082bd1f8ba182e36b19abd
Penelope Gwen / Pogmommy <penelope@pogmom.me>
2023-12-12 23:14:05 -0700

    Create action.sh

---
+++ action.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '6ED0E7B82643E131'
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '0E98404D386FA1D9'
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 'F8D2585B8783D481'
+echo "deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware" > /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bookworm main non-free-firmware" >> /etc/apt/sources.list
+sudo apt update
+
+sudo apt list -a debhelper
+
+sudo apt install debhelper
+
+sudo ./build.sh
