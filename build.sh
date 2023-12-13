7f124ad0b4fc8febe59a181a3cc433919ac46fc9
Penelope Gwen / Pogmommy <penelope@pogmom.me>
2023-12-12 23:16:02 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-#./clean.sh
-sudo whoami
 
-cat /etc/apt/sources.list
-cat /etc/apt/sources.list.d/*
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '6ED0E7B82643E131'
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '0E98404D386FA1D9'
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 'F8D2585B8783D481'
-echo "deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware" > /etc/apt/sources.list
-echo "deb http://deb.debian.org/debian/ bookworm main non-free-firmware" >> /etc/apt/sources.list
-sudo apt update --allow-unauthenticated
+./clean.sh
 
 sudo apt source base-files/stable
 
