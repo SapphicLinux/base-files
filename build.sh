79b80ad88a025f1224fb6c649b172b43dd56279b
Penelope Gwen / Pogmommy <penelope@pogmom.me>
2023-12-12 22:58:41 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ cat /etc/apt/sources.list.d/*
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '6ED0E7B82643E131'
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com '0E98404D386FA1D9'
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 'F8D2585B8783D481'
-echo "deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware" > /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bookworm main non-free-firmware" >> /etc/apt/sources.list
 sudo apt update --allow-unauthenticated
 
 sudo apt source base-files/stable
